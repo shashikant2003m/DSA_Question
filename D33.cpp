b91857c2e34436fe5ceaac10a086fb04007f3a3c
Shashikant Kumar <shashikant2003m@gmail.com>
2026-04-15 09:44:56 +0530

    updated

--- D33.cpp
+++ D33.cpp
@@ -26,4 +26,26 @@ int main() {
     
 }
 
-/
+// Reverse a string
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "hello";
+
+    int start = 0;
+    int end = str.length() - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << str;
+
+    return 0;
+}
